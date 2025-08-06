0eb0b80926bc8abb7376b2cc60cc9ecc5fb60d4d
Michelle Abt <mabt924@gmail.com>
2025-08-06 14:55:36 -0700

    Initial Commit

---
+++ InteractiveUIinSF/InteractiveUIinSFApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIinSFApp.swift
+//  InteractiveUIinSF
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIinSFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
